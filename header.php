3030f31db48f78658e5720ee40abf5d2bd97a8b7
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-01-09 12:12:42 +0000

    Setup lecture notes page

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
 			<li class="nav-item active">
 				<a class="nav-link" href="/clock">Clock</a>
 			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="/lecturenotes">Lecture Notes</a>
+			</li>
 			<li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Small projects</a>
 
---
+++ lecturenotes/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lecture Notes</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../static/styles/common.css">
+        <link rel="stylesheet" href="../static/styles/portfolio.css">
+    </head>
+
+    <body>
+        <?php include("../header.php"); ?>
+
+        <div class="container-fluid" style="padding: 0px;">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Lecture Notes</h2>
+                        <p>
+                            Here you will find the lecture notes I created while studying Computer Science at the University Of Sussex,
+                            I hope they help other students, or anyone to gain a better understanding of certain topics.
+                        </p>
+                    </div>
+                    
+<pre class="contents-list">
+First year
+    <a href="Mathematical Concepts">Mathematical Concepts</a>
+    <a href="Introduction to Multimedia">Introduction to Multimedia</a>
+</pre>
+                </div>
+            </div>
+        </div>
+
+        <?php include("../footer.php"); ?>
+    </body>
+</html>

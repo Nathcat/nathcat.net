1bd66e99bf9345af242cb5e8cca5dcdad3803630
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-17 00:23:51 +0100

    Changed cookie domain

--- user/index.php
+++ user/index.php
@@ -30,7 +30,7 @@
                 
                 <?php 
                 $session_name = session_name("AuthCat-SSO");
-                session_set_cookie_params(0, '/', '.nathcat.net');
+                session_set_cookie_params(0, '/', '*.nathcat.net');
                 session_start();
 
                 if (array_key_exists("login-error", $_SESSION)) {

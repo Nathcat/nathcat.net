281f3b00e3e52ec326f804cabc8b96dd2fe5cf21
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-01-05 00:10:29 +0000

    Updated network page to be more descriptive of the features of the network

--- network/index.php
+++ network/index.php
@@ -16,18 +16,89 @@
             <?php include("../header.php"); ?>
 
             <div class="main align-center">
+                <h1>The Nathcat Network</h1>
+
+                <h1><a href="https://apps.nathcat.net">Access the network</a></h1>
+                
                 <div class="content-card">
-                    <h2>What is the Nathcat Network?</h2>
+                    <h2>What is The Nathcat Network?</h2>
                     <p>
-                        The Nathcat Network is a collection of web applications
-                        I have created either for entertainment, or general
-                        utility.
-                    </p>
-                    <p>
-                        You can access the network through the apps page linked below.
+                        This is the name I have given to the collection of web
+                        applications and services I have created. It is a work
+                        in progress but there is a number of them available.
+                        Keep reading to find out more about each one!
                     </p>
                 </div>
-                <h1><a href="https://apps.nathcat.net">Access the network</a></h1>
+
+                <div class="content-card">
+                    <div class="column justify-center align-center">
+                        <img width=100 height=100 src="https://data.nathcat.net/sso/AuthCat.png">
+
+                        <h2>AuthCat</h2>
+                    </div>
+
+                    <p>
+                        AuthCat is a single-sign-on service I created for other
+                        applications and services on the network, and other
+                        associated applications.
+                    </p>
+                    <p>
+                        It facilitates authentication, provides basic user data
+                        and key management in a secure, reliable, and efficient
+                        manner.
+                    </p>
+                </div>
+
+                <div class="content-card">
+                    <div class="column justify-center align-center">
+                        <img width=100 height=100 src="https://apps.nathcat.net/static/images/peoplecat-logo.png">
+
+                        <h2>PeopleCat</h2>
+                    </div>
+
+                    <p>
+                        PeopleCat is a social media network built for simplicity,
+                        and ease of use.
+                    </p>
+                    <p>
+                        The origins of PeopleCat actually go back a long way,
+                        it started with the development of my A-Level 
+                        coursework project and it's prototypes, MessageCat.
+                        Although developing MessageCat helped me score very
+                        highly in my coursework, it did contain a number of flaws
+                        as a genuine production application, and was never fully
+                        released. I did start work on a second production ready
+                        version of the application but never got round to
+                        finishing it properly.
+                    </p>
+                    <p>
+                        PeopleCat was the successor to MessageCat, its'
+                        inception coming almost a year after the original version
+                        of MessageCat, and has now been almost a year in on-and-off
+                        development. Since I started developing this cluster of
+                        web applications I have teamed up with <a href="https://github.com/brooke-ec">Brooke Reavell</a>,
+                        and together with her work on the front-end web application, and my
+                        development of the back-end server we are moving quickly toward
+                        a ready application!
+                    </p>
+                    <p>
+                        At the moment, you can access my prototype web client for
+                        the basic messaging system in a simple global group chat
+                        through the networks <a href="https://apps.nathcat.net">home page</a>
+                    </p>
+                </div>
+
+                <div class="content-card">
+                    <div class="column align-center justify-center">
+                        <h2>SudokuCat</h2>
+                    </div>
+
+                    <p>
+                        SudokuCat is a simple Sudoku puzzle game, not much else to it!
+                        It features a leaderboard for total puzzles solved, and the current
+                        longest streak.
+                    </p>
+                </div>
             </div>
 
             <?php include("../footer.php"); ?>

f26f00f7248f304acd537940f2c7a4cf2165f910
Nathan Baines <nathan.baines@nathcat.net>
2025-11-12 22:41:39 +0000

    Changed to use curl

--- static/docs/Marcus-TTS/share/index.php
+++ static/docs/Marcus-TTS/share/index.php
@@ -1,20 +1,24 @@
 <?php 
 if (array_key_exists("text", $_GET)) {
-    $data = [
+    $data = json_encode([
         "voice" => "en_male_narration",
         "text" => $_GET["text"]
-    ];
+    ]);
 
-    $options = [
-        "http" => [
-            "header" => "Content-Type: application/json",
-            "method" => "POST",
-            "content" => json_encode($data)
-       ]
-    ];
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://ottsy.weilbyte.dev/api/generation");
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $headers = array(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    );
 
-    $context = stream_context_create($options);
-    $result = file_get_contents("https://ottsy.weilbyte.dev/api/generation", false, $context);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    $result = curl_exec($curl);
+    curl_close($curl);
+
     if ($result === false) {
         header("Content-Type: text/plain");
         echo "Failed to get text from API";

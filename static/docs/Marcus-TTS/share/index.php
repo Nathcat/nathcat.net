e0ae16542a514d90bcc3002699c45a02125e1021
Nathan Baines <nathan.baines@nathcat.net>
2025-11-12 22:32:18 +0000

    Added marcus tts endpoint

---
+++ static/docs/Marcus-TTS/share/index.php
@@ -0,0 +1,38 @@
+<?php 
+if (array_key_exists("text", $_GET)) {
+    $data = [
+        "voice" => "en_male_narration",
+        "text" => $_GET["text"]
+    ];
+
+    $options = [
+        "http" => [
+            "header" => "Content-Type: application/json\r\n",
+            "method" => "POST",
+            "content" => http_build_query($data)
+       ]
+    ];
+
+    $context = stream_context_create($options);
+    $result = file_get_contents("https://ottsy.weilbyte.dev/api/generation", false, $context);
+    if ($result === false) {
+        headers("Content-Type: text/plain");
+        echo "Failed to get text from API";
+    }
+    else {
+        $result = json_decode($result);
+        if ($result["success"] === false) {
+            headers("Content-Type: text/plain");
+            echo "Failed to get text from API: " . $result["error"];
+        }
+        else {
+            headers("Content-Type: audio/mpeg");
+            echo base64_decode($result["data"]);
+        }
+    }
+}
+else {
+    headers("Content-Type: text/plain");
+    echo "Must specify text parameter.";
+}
+?>
